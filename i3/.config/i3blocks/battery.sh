e704e0beb84aa4744fc33b64602e7818198ac04d
chrisrhayden <chrisrhayden@gmail.com>
2023-11-26 05:44:41 -0800

    update battery script

--- i3/.config/i3blocks/battery.sh
+++ i3/.config/i3blocks/battery.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ $(hostnamectl hostname) == "Odimm" ]]; then
-    BAT="$(acpi -b | grep --perl-regexp --only-matching '\d\d?%')"
+    BAT="$(cat /sys/class/power_supply/BAT1/capacity)"
+    STATUS="$(cat /sys/class/power_supply/BAT1/status)"
 
-    printf 'bat %s\n' "$BAT"
-    printf 'bat %s\n' "$BAT"
+    printf 'bat %s%%\n' "$BAT"
+    printf 'bat %s%%\n' "$BAT"
 
-    if [[ ${BAT%?} -le 5 ]]; then
+    if [[  $STATUS == "Charging" ]]; then
+        printf '#00ff00\n'
+    elif (($BAT < 5)); then
         exit 33
-    elif [[ ${BAT%?} -le 20 ]]; then
+    elif (($BAT < 20)); then
         printf '#ff8000\n'
     fi
 fi
