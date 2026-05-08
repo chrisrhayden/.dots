0c8c1b4188a7321d2aeb69e43a35c3cd51ad48d4
chrisrhayden <chrisrhayden@gmail.com>
2026-05-08 14:19:02 -0700

    day to day

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-login
     # set --global GBM_BACKEND=nvidia-drm
     # set --global __GLX_VENDOR_LIBRARY_NAME=nvidia
     if test \( -z "$DISPLAY"  \) -a \( "$XDG_VTNR" -eq "1" \)
-        Hyprland
+        start-hyprland
     end
 end
 
